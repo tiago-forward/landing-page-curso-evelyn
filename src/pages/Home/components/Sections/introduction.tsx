7644fa91eee3c8f8f37463460ab1be5fe1471c7c
tiagoforward <devtiagoforward@gmail.com>
2024-07-06 17:04:17 -0300

    added image when changing section

--- src/pages/Home/components/Sections/introduction.tsx
+++ src/pages/Home/components/Sections/introduction.tsx
@@ -4,12 +4,12 @@ export function Introduction() {
     return (
         <div className="max-w-6xl mx-auto min-h-[79vh] p-4 pt-40">
             <div className="flex flex-col items-end">
-                <h1 className="max-w-2xl mb-20 text-pr-1 text-4xl lg:text-5xl text-end font-bold font-gloock lg:tracking-wider">
+                <h1 className="z-10 max-w-2xl mb-20 text-pr-1 text-4xl lg:text-5xl text-end font-bold font-gloock lg:tracking-wider">
                     Venha participar do meu novo curso, onde irei te ajudar a <span className="text-amber-900">faturar</span> e se tornar uma maquiadora <span className="text-amber-900">profissional</span>
                 </h1>
             </div>
             <div className="flex flex-col items-center justify-center gap-4">
-                <p className="text-center text-lg text-amber-900 tracking-wider font-robotoSlab">Entre em contato comigo ou continue rolando para saber mais</p>
+                <p className="z-10 text-center text-lg text-amber-900 tracking-wider font-robotoSlab">Entre em contato comigo ou continue rolando para saber mais</p>
                 <CallToActionAnchor href='' />
             </div>
         </div>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,16 @@
 import { Introduction } from "./components/Sections/introduction";
 import { Skills } from "./components/Sections/skills";
 
+import Wave01 from '../../assets/wave-01.svg'
+
 export function Home() {
   return (
     <main className="">
       <section className="backgroundIntroduction min-h-[79vh] bg-pr-1 bg-cover bg-no-repeat bg-left md:bg-center">
         <Introduction />
+        <div className="relative w-full">
+          <img className="absolute bottom-0 left-0" src={Wave01} alt="" />
+        </div>
       </section>
       <section className="bg-pr-2 min-h-screen py-10">
         <Skills />
