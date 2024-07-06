c8775e262fb5e1a00117895c7a9d77e06fd39d00
tiagoforward <devtiagoforward@gmail.com>
2024-07-06 13:07:57 -0300

    adding new styles

---
+++ src/pages/Home/components/Sections/introduction.tsx
@@ -0,0 +1,7 @@
+export function Introduction() {
+  return (
+    <div className="max-w-6xl mx-auto p-4">
+        
+    </div>
+  )
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,18 @@
+import { Introduction } from "./components/Sections/introduction";
+import { Skills } from "./components/Sections/skills";
+
 export function Home() {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <main className="">
+      <section className="bg-pr-1 min-h-screen">
+        <Introduction />
+      </section>
+      <section className="bg-pr-2 min-h-screen py-10">
+        <Skills />
+      </section>
+      <section className="bg-white min-h-screen py-10">
+
+      </section>
+    </main>
   )
 }
