1be63365e7fa433211d6077eadf5e29c00580acf
tiagoforward <devtiagoforward@gmail.com>
2024-07-08 10:55:56 -0300

    added background to customer imagem

--- src/pages/Home/components/Sections/introduction.tsx
+++ src/pages/Home/components/Sections/introduction.tsx
@@ -1,15 +1,20 @@
 import { CallToActionAnchor } from '../../../../components/CallToActionAnchor'
 
+import ImageTeste from '../../../../../../../../../../Pictures/pngegg.png'
+
 export function Introduction() {
     return (
-        <div className="max-w-6xl mx-auto min-h-[79vh] p-4 pt-40">
+        <div className="max-w-6xl mx-auto min-h-[85vh] p-4 pt-40">
+            <div className='rounded-custom absolute bg-blue-700 w-[28rem] h-[28rem] overflow-hidden'>
+                <div className='h-full w-full object-cover'><img className='absolute' src={ImageTeste} alt="" /></div>
+            </div>
             <div className="flex flex-col items-end">
                 <h1 className="max-w-2xl mb-20 text-pr-1 text-4xl lg:text-5xl text-end font-bold font-gloock lg:tracking-wider">
                     Venha participar do meu novo curso, onde irei te ajudar a <span className="text-amber-900">faturar</span> e se tornar uma maquiadora <span className="text-amber-900">profissional</span>
                 </h1>
             </div>
-            <div className="flex flex-col items-center justify-center gap-4">
-                <p className="z-10 text-center text-lg text-amber-900 tracking-wider font-robotoSlab">Entre em contato comigo ou continue rolando para saber mais</p>
+            <div className="flex flex-col items-end justify-center gap-4">
+                <p className="z-10 text-lg text-amber-900 tracking-wider font-robotoSlab">Entre em contato comigo ou continue rolando para saber mais</p>
                 <CallToActionAnchor href='' />
             </div>
         </div>
